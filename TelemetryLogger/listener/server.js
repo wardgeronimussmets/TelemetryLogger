8b60ca577a5d5a0f827fbceee845ebdb668b7509
wardgeronimussmets <81919921+wardgeronimussmets@users.noreply.github.com>
2024-07-06 18:49:47 +0200

    added basic listener

---
+++ TelemetryLogger/listener/server.js
@@ -0,0 +1,22 @@
+const dgram = require('node:dgram');
+const server = dgram.createSocket('udp4');
+
+const PORT = 3000;
+const HOST = 'localhost';
+
+server.on('listening', () => {
+    const address = server.address();
+    console.log(`UDP Server listening on ${address.address}:${address.port}`);
+});
+
+server.on('message', (message, remote) => {
+    console.log(`Received message from ${remote.address}:${remote.port}`);
+    console.log(`Message: ${message}`);
+});
+
+server.on('error', (err) => {
+    console.error(`Server error:\n${err.stack}`);
+    server.close();
+});
+
+server.bind(PORT, HOST);
